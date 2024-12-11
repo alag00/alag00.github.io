c7cc189bdedc3e4f2f46ace773f9543b31bd9dfe
AlagPC <agfemalm@hotmail.com>
2024-12-11 03:16:37 +0100

    Videos, Projects, Navbar, About
    Replaced all youtube embeded links with mp4 embeded videos. Added BetweenTheSignals to small group projects. Added "outline" around the embeded video. Altered the Portfolio buttons. Replaced the lorem ipsum text in About, also added background to the text.

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ function OpenLarge(num) {
     x.style.display = "block";
     y.style.display = "none";
     z.style.display = "none";
+
+    var a = document.getElementById("groupLargeButton");
+    var b = document.getElementById("groupSmallButton");
+    var c = document.getElementById("groupSoloButton");
+
+    a.style.background = "rgb(30, 20, 20)";
+    b.style.background = "rgb(10, 15, 10)";
+    c.style.background = "rgb(10, 10, 15)";
+
+    a.style.color = "yellow";
+    b.style.color = "gray";
+    c.style.color = "gray";
+
 }
 
 function OpenSmall(num) {
@@ -16,6 +29,18 @@ function OpenSmall(num) {
     x.style.display = "none";
     y.style.display = "block";
     z.style.display = "none";
+
+     var a = document.getElementById("groupLargeButton");
+     var b = document.getElementById("groupSmallButton");
+     var c = document.getElementById("groupSoloButton");
+
+     a.style.background = "rgb(15, 10, 10)";
+     b.style.background = "rgb(20, 30, 20)";
+     c.style.background = "rgb(10, 10, 15)";
+
+     a.style.color = "gray";
+     b.style.color = "yellow";
+     c.style.color = "gray";
 }
 
 function OpenSolo(num) {
@@ -26,4 +51,17 @@ function OpenSolo(num) {
     x.style.display = "none";
     y.style.display = "none";
     z.style.display = "block";
+
+         var a = document.getElementById("groupLargeButton");
+         var b = document.getElementById("groupSmallButton");
+         var c = document.getElementById("groupSoloButton");
+
+         a.style.background = "rgb(15, 10, 10)";
+         b.style.background = "rgb(10, 15, 10)";
+         c.style.background = "rgb(20, 20, 30)";
+
+         a.style.color = "gray";
+         b.style.color = "gray";
+         c.style.color = "yellow";
 }
+
