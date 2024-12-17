484267e59ffe4e4627a3c8a639ae124240530767
AlagPC <agfemalm@hotmail.com>
2024-12-18 00:41:56 +0100

    Introduction now shows gameplay

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+
+ function displayNextVideo() {
+          x = (x === videos.length - 1) ? 0 : x + 1;
+          document.getElementById("gameplayPreview").src = videos[x];
+      }
+      function changeVideo() {
+          setInterval(displayNextVideo, 15000);
+      }
+
+      var videos = [], x = 0;
+      videos[0] = "videos/PreviewCB.mp4#t=30";
+      videos[1] = "videos/PreviewTT.mp4#t=37";
+      videos[2] = "videos/PreviewUT.mp4#t=30";
+
+/*
+function ChangeVideoSource(currentIndex)
+{
+    var vid = document.getElementById("introBackground");
+    currentIndex++;
+
+    switch(currentIndex)
+    {
+    case 1:
+    vid.src="videos/PreviewCB.mp4";
+    break;
+
+     case 2:
+      vid.src="videos/PreviewCP.mp4";
+        break;
+
+         case 3:
+          vid.src="videos/PreviewUT.mp4";
+          currentIndex = 0;
+            break;
+
+    }
+}
+*/
+
 function OpenLarge(num) {
   var x = document.getElementById("largeProjects");
   var y = document.getElementById("smallProjects");
@@ -15,7 +54,7 @@ function OpenLarge(num) {
     b.style.background = "rgb(10, 15, 10)";
     c.style.background = "rgb(10, 10, 15)";
 
-    a.style.color = "yellow";
+    a.style.color = "rgb(180,180,40)";
     b.style.color = "gray";
     c.style.color = "gray";
 
@@ -39,7 +78,7 @@ function OpenSmall(num) {
      c.style.background = "rgb(10, 10, 15)";
 
      a.style.color = "gray";
-     b.style.color = "yellow";
+     b.style.color = "rgb(180,180,40)";
      c.style.color = "gray";
 }
 
@@ -62,6 +101,6 @@ function OpenSolo(num) {
 
          a.style.color = "gray";
          b.style.color = "gray";
-         c.style.color = "yellow";
+         c.style.color = "rgb(180,180,40)";
 }
 
